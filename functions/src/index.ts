fada5cd9a606ec21ff6f2c90d4d4dfce363a4bd4
KrishnaDesktop <krishna4132@gmail.com>
2021-02-24 00:10:07 -0500

    cloud function fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,23 +5,30 @@ admin.initializeApp();
 export const createChat = functions.firestore.document("/chatSearch/searchingUsers").onUpdate( (change) => {
   const data = change.after.data();
   const listOfUsers: string[] = data.liveSearchingUsers;
+  functions.logger.log(listOfUsers.toString());
   if (listOfUsers.length > 1) {
-    functions.logger.log(listOfUsers.length);
-    functions.logger.log("Test Log");
-    const uid0 = listOfUsers[0];
-    const uid1 = listOfUsers[1];
-    const chat = {
-      users: [uid0, uid1],
-      createdAt: admin.database.ServerValue.TIMESTAMP,
-      messages: [],
-      usersTyping: {
-        uid0: false,
-        uid1: false,
-      },
-    };
-    admin.firestore().collection("chats").add(chat);
-    admin.firestore().doc("/chatSearch/searchingUsers").update({
-      liveSearchingUsers: admin.firestore.FieldValue.arrayRemove([uid0, uid1]),
-    });
+    const uid0: string = listOfUsers[0];
+    const uid1: string = listOfUsers[1];
+    const users: string[] = [uid0, uid1];
+    try {
+      const chat = {
+        users: users,
+        createdAt: admin.database.ServerValue.TIMESTAMP,
+        messages: [],
+        usersTyping: {
+          [uid0]: false,
+          [uid1]: false,
+        },
+      };
+      admin.firestore().collection("chats").add(chat);
+      admin.firestore().doc("/chatSearch/searchingUsers").update({
+        liveSearchingUsers: admin.firestore.FieldValue.arrayRemove(uid0),
+      });
+      admin.firestore().doc("/chatSearch/searchingUsers").update({
+        liveSearchingUsers: admin.firestore.FieldValue.arrayRemove(uid1),
+      });
+    } catch (e) {
+      functions.logger.log(e);
+    }
   }
 });
